80509cd7fc356486148633a38a7f46f7726a8808
dhaniel aritonang <dhtamado@gmail.com>
2020-05-28 13:09:50 +0700

    Adding a row to remove whitespaces, imporove wordcloud pattern and so on

--- Twitter_analysis.R
+++ Twitter_analysis.R
@@ -2,7 +2,7 @@ library(rtweet)
 library(tidyverse)
 library(ggplot2)
 
-#Retrieving data from TWITTER API
+#Connect to Twitter API
 twitter_token <- create_token(
   app = "dhtamado",
   consumer_key = "C0AIGCi79i6FYaoxde5Btb8Ya",
@@ -12,29 +12,32 @@ twitter_token <- create_token(
   set_renv = TRUE
 )
 
-#Mengambil tweet dari akun pak jokowi
+#Mengambil tweet dari akun pak jokowi 
+# (nilai n = 3200 menunjukkan jumlah maksimal tweets yang bisa diambil oleh Twitter)
 jokowi <- get_timeline("@jokowi", n = 3200)
 
 #Membuang retweet
 jokowi_organic_tweet <- jokowi[jokowi$is_retweet == FALSE,]
 
-#Mmebuang tweet reply
+#Membuang tweet reply
 jokowi_organic_tweet <- subset(jokowi_organic_tweet, is.na(jokowi_organic_tweet$reply_to_status_id))
+view(jokowi_organic_tweet)
 
-#Analisa engagement akun twitter Jokowi
+#Analisa engagement akun twitter Jokowi (Like dan Retweet)
 # 1.Melihat tweet Jokowi dengan urutan yang paling banyak di Likes
 likes <- jokowi_organic_tweet %>% arrange(-favorite_count)
+
 # 2.Melihat tweet Jokowi yang paling banyak di retweet (descending)
 retweet <- jokowi_organic_tweet %>% arrange(-retweet_count)
 
-
-
 #Analisa ratio reply/likes/organic tweets
-#Mebuat dataframe  hanya retweet
-jokowi_retweet <- jokowi[jokowi$is_retweet == "FALSE", ]
+#Mebuat dataframe hanya retweet
+jokowi_retweet <- jokowi[jokowi$is_retweet == "TRUE", ]
+view(jokowi_retweet)
 
 #Membuat dataframe hanya reply
 jokowi_reply <- subset(jokowi, !is.na(jokowi$reply_to_status_id))
+
 
 #membuat data jumlah masing2 (organic, retweet, reply)
 n_jkw_organic <- nrow(jokowi_organic_tweet)
@@ -45,43 +48,44 @@ analisis <- data.frame(
   Kategori = c("Organic", "Retweet", "Reply"),
   Jumlah = c(n_jkw_organic,n_retweet,n_reply)
 )
+#Menampilkan barplot Perbandingan Jumlah Organic vs Retweet vs Reply Tweet
 library(ggplot2)
 ggplot(data = analisis) + geom_bar(mapping = aes(x = Kategori, y = Jumlah, fill = Kategori), stat = "identity")
 
-#Membersihkan tweet dari berbagai hal yang tak diinginkan
+#Membersihkan tweet dari berbagai hal yang tak diinginkan (link url dan tanda baca)
 jokowi_organic_tweet$text <- gsub("https\\S*", "", jokowi_organic_tweet$text)
 jokowi_organic_tweet$text <- gsub("@\\S*", "", jokowi_organic_tweet$text)
 jokowi_organic_tweet$text <- gsub("amp", "", jokowi_organic_tweet$text)
 jokowi_organic_tweet$text <- gsub("[\r\n]", "", jokowi_organic_tweet$text)
 jokowi_organic_tweet$text <- gsub("[[:punct:]]", "", jokowi_organic_tweet$text)
-class(jokowi_organic_tweet$text)
 
+
+#Menghilangkan kata sambung, kata panggil dll (list dibuat sendiri)
 library(tidytext)
 library(tm)
 library(tidyverse)
 tweets <- jokowi_organic_tweet %>% select(text)
-x = readLines('C://Users/LENOVO/Desktop/stopwords.txt')
+tweets <- lapply(tweets, tolower)
+x = readLines('C://Users/LENOVO/Desktop/STEM/stopwords.txt') #List yang mengandung kata-kata sambung/panggilan yang dibuat sendiri
 y = tweets$text
-y = removeWords(y,x)
+y = removeWords(y,x) %>% str_squish()
 tweets$text_new <- y
-tweets$text_new
+tweets <- as_tibble(tweets)
+tweets <- tweets %>% select(text_new) %>%
+  unnest_tokens(word, text_new)
 
-tweets <- tweets %>% 
-  select(text_new) %>%
-  unnest_tokens(word,text_new)
-view(tweets)
 
-tweets %>% # gives you a bar chart of the most frequent words found in the tweets
+#Melakukan plot yang menghasilkan kata-kata yang sering di-Tweet oleh Presiden Jokowi
+tweets %>% 
   count(word, sort = TRUE) %>%
   top_n(15) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot() + 
   geom_bar(mapping = aes(x = reorder(word,n), y = n), stat = "identity", fill = "blue") + coord_flip() +
   labs(x = "Kata unik", y = "Jumlah Tweet", title = "Kata yang sering di-Tweet oleh Presiden Jokowi", subtitle = "*Kata-kata sambung telah dihilangkan")
-d <- as.data.frame(tweets)
-as.data.frame(table(d)) %>% arrange(desc(Freq)) %>% view()
 
+#Membuat wordcloud yang berisi kata-kata unik dari tweet Jokowi
 library(wordcloud)
 library(RColorBrewer)
 set.seed(1234)
-wordcloud(tweets$word, min.freq = 1, max.words = 300, random.order = FALSE, rot.per=0.35, colors = brewer.pal(8,"Dark2"))
+wordcloud(tweets$word, min.freq = 1, max.words = 150, random.order = FALSE, rot.per=0.35, colors = brewer.pal(8,"Dark2"))
